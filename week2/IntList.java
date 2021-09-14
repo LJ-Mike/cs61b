e7b34d01a87cade6d286b4a448fe44a402ac6b4d
Nguyen Huu Quan <githubProgramming@gmail.com>
2021-09-14 11:46:56 +0700

    Do exercise 2.1

---
+++ week2/IntList.java
@@ -0,0 +1,60 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+    /**
+     * Constructor
+     * @param f int
+     * @param r IntList
+     */
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+    /**
+     * Using recursion
+     * @return size of IntList
+     */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + rest.size();
+    }
+    /**
+     * Not using recursion
+     * @return size of IntListt
+     */
+    public int iterativeSize() {
+        IntList r = rest;
+        int ret = 1;
+        while (r != null) {
+            ret = ret + 1;
+            r = r.rest;
+        }
+        return ret;
+    }
+    /**
+     * Using recursion
+     * @param i index
+     * @return IntList at index i
+     */
+    public IntList get(int i) {
+        if (i == 0) {
+            return this;
+        }
+        return rest.get(i - 1);
+    }
+    /**
+     * Not using recursion
+     * @param i index
+     * @return IntList at index i
+     */
+    public IntList iterativeGet(int i) {
+        IntList t = this;
+        while (i > 0) {
+            t = t.rest;
+            i = i - 1;
+        }
+        return t;
+    }
+}
